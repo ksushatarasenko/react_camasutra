a572aa47c8cfb57573d5e049e1a2cc77e1aff6bd
Oksana <ksushatarasenko@gmail.com>
2023-12-22 10:14:00 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App(props) {
   
     return (
       <div className='wrapper'>
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
           <Header/>
           <Nav/>
             <Routes>
