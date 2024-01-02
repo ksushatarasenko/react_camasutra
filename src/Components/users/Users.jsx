55d5bede605727fd1a0665a945979d34c6a05fba
Oksana <ksushatarasenko@gmail.com>
2024-01-02 08:25:24 +0100

    lesson 49

--- src/Components/users/Users.jsx
+++ src/Components/users/Users.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import user from './users.module.css'
 
 function Users(props) {
+    console.log(props)
+    // if(props.users.length === 0){
+    //     props.setUsers(
+    //         [
+    //             { id: 1, photoUrl:'', fololowed: false, name:"Petr", status: 'I am a boss', location: {city: 'Oster', country: 'Ukraine'}},
+    //             { id: 2, photoUrl:'', fololowed: true,name:"Oksana", status: 'I am a boss', location: {city: 'Oksana', country: 'Ukraine'}},
+    //             { id: 3, photoUrl:'', fololowed: true, name:"Katya", status: 'I am a boss', location: {city: 'Chernihov', country: 'Ukraine'}},
+    //             { id: 4, photoUrl:'', fololowed: false, name:"Maryna", status: 'I am a boss', location: {city: 'Chernihov', country: 'Ukraine'}},
+    //             { id: 5, photoUrl:'', fololowed: true, name:"Kyrylo", status: 'I am a boss', location: {city: 'Lutava', country: 'Ukraine'}},
+    //             { id: 6, photoUrl:'', fololowed: true, name:"Ivan", status: 'I am a boss', location: {city: '', country: ''}},
+    //             { id: 7, photoUrl:'', fololowed: false, name:"Ira", status: 'I am a boss', location: {city: '', country: ''}},
+    //             { id: 8, photoUrl:'', fololowed: true, name:"Maria", status: 'I am a boss', location: {city: '', country: ''}},
+    //             { id: 9, photoUrl:'', fololowed: false, name:"Igor", status: 'I am a boss', location: {city: '', country: ''}},
+            
+    //           ]
+    //     )
+    // }
+
   return (
-    <div>
+    <div className={user.wrapper}>
         {
-            props.users.map(u => <div key={u.id}>
+            props.users.map(u => <div key={u.id} className={user.continer}>
                 <span>
-                    <div><img src="" alt="" /></div>
+                    <div><img src={u.photoUrl} alt="" /></div>
                     <div><button>Follow</button></div>
                 </span>
                 <span>
